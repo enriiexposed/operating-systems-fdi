22bb40ad727170224a258c704328bea804819ccf
enriiexposed <rios04enrique@gmail.com>
2025-11-30 22:08:37 +0100

    Añadir archivos binarios y mejorar la legibilidad del código en read_strings.c y write_strings.c

--- practicas/ficheros_p2/ejercicio2/read_strings.c
+++ practicas/ficheros_p2/ejercicio2/read_strings.c
@@ -14,38 +14,43 @@ returns
 *
 * Returns: !=NULL if success, NULL if error
 */
-char *loadstr(FILE *file)
-{
+char *loadstr(FILE *file) {
 	int length = 0;
 	int init = ftell(file);
 	int c;
-	while ((c = fgetc(file)) != EOF && c != '\0')
-	{
+	while ((c = fgetc(file)) != EOF && c != '\0') {
 		length++;
 	}
-	if (c == EOF)
-	{
+
+	if (c == EOF) {
 		return NULL;
 	}
+
 	fseek(file, init, SEEK_SET);
-	char *str = (char *)malloc((length + 1) * sizeof(char));
+	char *str = (char *) malloc((length + 1) * sizeof(char));
 	fread(str, sizeof(char), length + 1, file);
 	return str;
 }
-int main(int argc, char *argv[])
-{
-	FILE *file;
-	if ((file = fopen(argv[1], "rb")) == NULL)
-	{
-		fprintf(stderr, "The file could not be opened: ");
-		perror(NULL);
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		return 2;
 	}
+
+	FILE *file = fopen(argv[1], "rb");
+	if (file == NULL) {
+		fprintf(stderr, "The file could not be opened: ");
+		perror(argv[1]);
+		return 1;
+	}
+
 	char *str;
-	while ((str = loadstr(file)) != NULL)
-	{
+	while ((str = loadstr(file)) != NULL) {
 		printf("%s\n", str);
 		free(str);
 	}
+
 	fclose(file);
 	return 0;
 }
--- practicas/ficheros_p2/ejercicio2/write_strings.c
+++ practicas/ficheros_p2/ejercicio2/write_strings.c
@@ -2,23 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <err.h>
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		fprintf(stderr, "Usage: <file_name> <strngs> ...\n");
 		exit(EXIT_FAILURE);
 	}
 
 	FILE *file;
-	if ((file = fopen(argv[1], "wb")) == NULL)
-	{
+	if ((file = fopen(argv[1], "wb")) == NULL) {
 		fprintf(stderr, "The file could not be opened: ");
 		perror(NULL);
 	}
-	for (int i = 2; i < argc; i++)
-	{
+
+	for (int i = 2; i < argc; i++) {
 		fwrite(argv[i], sizeof(char), strlen(argv[i]) + 1, file);
 	}
+
 	fclose(file);
 	return 0;
 }
